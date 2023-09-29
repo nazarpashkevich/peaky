a31c9e795cd0ea6fb62526ff1483db7dcf02ca64
Nazar <nazarpashkevich@gmail.com>
2023-09-29 12:05:05 +0100

    feature: slots

---
+++ src/components/about/AboutLayout.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+  <slot name="introduction"/>
+  <slot name="about"/>
+  <slot name="rewards">
+    Our rewards will be soon!
+  </slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: "AboutLayout"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,25 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <h2>Slots</h2>
+
+  <AboutLayout>
+      <template #introduction>
+        introduction
+      </template>
+      <template #about>
+        about about
+      </template>
+      <template #rewards>
+
+      </template>
+  </AboutLayout>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+<style></style>
+<script>export default {
+  components: {}
 }
-</style>
+</script>
+
+<script setup>
+import AboutLayout from "./../components/about/AboutLayout.vue";
+</script>
